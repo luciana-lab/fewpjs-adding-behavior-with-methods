d5639ac6679c02228bc48dcb4dc0c152fbf62d3f
Luciana Sugeta <lulusugeta@gmail.com>
2021-09-17 10:41:29 -0400

    commit tree 7fcbc48d6018eac22d07058cad064efe26428144

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Your code here
+// cat
+// 1) has a class of cat
+// 2) cat.speak() returns '[name]' says meow!
+class Cat {
+    constructor(name, sex) {
+        this.name = name;
+        this.sex = sex;
+    };
+
+    speak() {
+        return `${this.name} says meow!`
+    };
+};
+
+// dog
+// 1) has a class of dog
+// 2) dog.speak() returns '[name] says woof!'
+class Dog {
+    constructor(name, sex) {
+        this.name = name;
+        this.sex = sex;
+    };
+
+    speak() {
+        return `${this.name} says woof!`
+    };
+};
+
+// bird
+// 1) has a class of Bird
+// 2) bird.speak() returns 'It's me! [name], the parrot!'
+// 3) bird2.speak() returns 'Squawk!'
+class Bird {
+    constructor(name, sex) {
+        this.name = name;
+        this.sex = sex;
+    };
+
+    speak() {
+        return this.sex === 'male' ? `It's me! ${this.name}, the parrot!` : `${this.name} says squawk!`
+    };
+};
